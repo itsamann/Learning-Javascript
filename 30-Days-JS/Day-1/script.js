6c589257878e7615293dae0c5c2b03713e8cb22a
Aman Sharma <amansha2047@gmail.com>
2024-07-18 00:03:11 -0400

    Create a Simple Note Making Function

--- 30-Days-JS/Day-1/script.js
+++ 30-Days-JS/Day-1/script.js
@@ -1,49 +1,58 @@
-// Write a single line comment which says, comments can make code readable
+// // Write a single line comment which says, comments can make code readable
 
-// Write another single comment which says, Welcome to 30DaysOfJavaScript
+// // Write another single comment which says, Welcome to 30DaysOfJavaScript
 
-// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+// // Write a multiline comment which says, comments can make code readable, easy to reuse and informative
 
-// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// // Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
 
-// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// // Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
 
-// Declare four variables without assigning values
+// // Declare four variables without assigning values
 
-// Declare four variables with assigned values
+// // Declare four variables with assigned values
 
-// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// // Declare variables to store your first name, last name, marital status, country and age in multiple lines
 
-// Declare variables to store your first name, last name, marital status, country and age in a single line
+// // Declare variables to store your first name, last name, marital status, country and age in a single line
 
-// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+// // Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
 
-let a = "Aman";
-let b = true;
-let c = undefined;
-let d = null;
-let e = 25;
+// let a = "Aman";
+// let b = true;
+// let c = undefined;
+// let d = null;
+// let e = 25;
 
-console.log(
-  `String: ${a}, Boolean: ${b}, Undefined: ${c}, Null: ${d}, Number: ${e}`
-);
-console.log(
-  `String: ${typeof a}, Boolean: ${typeof b}, Undefined: ${typeof c}, Null: ${typeof d}, Number: ${typeof e}`
-);
+// console.log(
+//   `String: ${a}, Boolean: ${b}, Undefined: ${c}, Null: ${d}, Number: ${e}`
+// );
+// console.log(
+//   `String: ${typeof a}, Boolean: ${typeof b}, Undefined: ${typeof c}, Null: ${typeof d}, Number: ${typeof e}`
+// );
 
-// let firstName = "Aman";
-// let lastName = "Sharma";
-// let maritalSatus = false;
-// let country = "India";
-// let age = 25;
+// // let firstName = "Aman";
+// // let lastName = "Sharma";
+// // let maritalSatus = false;
+// // let country = "India";
+// // let age = 25;
 
-let firstName = "John",
-  lastName = "Doe",
-  maritalStatus = "single",
-  country = "USA",
-  age = 30;
+// let firstName = "John",
+//   lastName = "Doe",
+//   maritalStatus = "single",
+//   country = "USA",
+//   age = 30;
 
-let word = "JavaScript";
-word[0] = "Y";
+// let word = "JavaScript";
+// word[0] = "Y";
 
-console.log(word);
+// console.log(word);
+
+// Simple Note Making Fuction
+let userNotes = [];
+
+function notes(userInput) {
+  let userInput = prompt("Enter your note");
+  userNotes.push(userInput);
+  return console.log(userNotes);
+}
