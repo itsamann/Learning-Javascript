c2badfdcc2b83722baccbac0b09eb62015a82adc
Aman Sharma <amansha2047@gmail.com>
2024-07-19 01:27:46 -0400

    Revised Array Methods

--- Week1/index.js
+++ Week1/index.js
@@ -1,6 +1,6 @@
-// STRING METHODS
-let firstName = "Aman       ";
-let lastName = "Sharma";
+// *********************************************   STRING METHODS   *********************************************
+// let firstName = "Aman       ";
+// let lastName = "Sharma";
 
 // 1. Concatenation
 // let fullName = firstName + " " + lastName;
@@ -29,7 +29,64 @@ let lastName = "Sharma";
 // console.log(firstName.includes("A"));
 
 // 8. Trim
-console.log(firstName.trim());
+// console.log(firstName.trim()); // Remove Extra Space
+
+// *************************************************************************************************************
+
+// *********************************************   ARRAY METHODS   *********************************************
+const fruits = [
+  "Apples",
+  "Pomegranate",
+  "Mango",
+  "Strawberries",
+  "Pineapple",
+  "Grapes",
+];
+
+console.log(fruits.length);
+
+// 1. Push Method: It will add a new element to the end of an array and returns the new length of an array.
+fruits.push("Banana");
+console.log(fruits);
+
+// 2. Pop Method: It removes the last element of an array and returns the removed element.
+fruits.pop();
+console.log(fruits);
+
+// 3. Shift Method: It removes the first element of an array and returns the removed element.
+fruits.shift();
+console.log(fruits);
+
+// 4. Unshift Method: It will add a new element to the start of an array and returns the new length of an array.
+fruits.unshift("Orange");
+console.log(fruits);
+
+// 5. Concat Method: Join 2 or more arrays and returns a result.
+const moreFruits = ["Tomato", "Potato", "Pututu"];
+const newArray = fruits.concat(moreFruits);
+console.log(newArray);
+
+// 6. Include Method: Checks if an array contains a specified element.
+const pl = ["Javascript", "Java", "C++", "Python", "Rust"];
+const numbers = [2, 5, 1, 4, 3];
+
+console.log(pl.includes("Javascript"));
+
+// 7. Join Method: Join all the elements of an array into a string, seperated by a specified seperator.
+console.log(pl.join("-"));
+
+// 8. Reverse method: Reverse the elements in an array in place.
+console.log(pl.reverse());
+
+// 9. Slice Method: Selects the part of an array and returns the new array.
+console.log(pl.slice(0, 1));
+
+// 10. Sort Method: Sort the elements alphabeically in strings and in ascending order.
+console.log(numbers.sort());
+
+// ******************************************************************************************************************
+
+// *********************************************   PRACTICE QUESTIONS   *********************************************
 
 // let fname = prompt("What is your First Name?");
 // let lname =  prompt("What is your Last Name?");
